28841b7ca5d0c861df0dc08a12b7958d4322a10f
Chaimae Bougattaya <cbougattaya@smb.sqli.com>
2023-03-11 15:49:36 +0100

    Initial commit

---
+++ src/main/java/com/sqli/train/Train.java
@@ -0,0 +1,24 @@
+package com.sqli.train;
+
+public class Train {
+
+	public Train(String structure) {
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public String print(){
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public void detachEnd(){
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public void detachHead(){
+		throw new UnsupportedOperationException("Still to be implemented");
+	}
+
+	public boolean fill(){
+        throw new UnsupportedOperationException("Still to be implemented");
+	}
+}
