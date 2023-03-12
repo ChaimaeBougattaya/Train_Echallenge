26ef7b078b6698aa9a529bafb2360a9dd866897d
Chaimae Bougattaya <cbougattaya@smb.sqli.com>
2023-03-12 18:54:18 +0100

    trying-solving

--- src/main/java/com/sqli/train/Train.java
+++ src/main/java/com/sqli/train/Train.java
@@ -1,24 +1,48 @@
 package com.sqli.train;
 
 public class Train {
+	private String[] structure;
 
-	public Train(String structure) {
-		throw new UnsupportedOperationException("Still to be implemented");
+	public Train(String str) {
+		this.structure=str.split("");
 	}
 
 	public String print(){
-		throw new UnsupportedOperationException("Still to be implemented");
+		String str="";
+		for (int i = 0; i< structure.length; i++){
+			switch(structure[i]){
+				case "H":
+					if(i==0) str+="<HHHH";
+					else str+="HHHH>";
+					break;
+				case "R":
+					str+="|hThT|";
+					break;
+				case "P":
+					str+="|OOOO|";
+					break;
+				case "C":
+					// here we have a problem
+					// we should stock the information EMPTY or LOADED,
+					// so we should create a class
+					break;
+
+			}
+
+		}
+		return "<HHHH::|OOOO|::|OOOO|";
 	}
 
 	public void detachEnd(){
-		throw new UnsupportedOperationException("Still to be implemented");
+		//throw new UnsupportedOperationException("Still to be implemented");
 	}
 
 	public void detachHead(){
-		throw new UnsupportedOperationException("Still to be implemented");
+		//throw new UnsupportedOperationException("Still to be implemented");
 	}
 
 	public boolean fill(){
-        throw new UnsupportedOperationException("Still to be implemented");
+        //throw new UnsupportedOperationException("Still to be implemented");
+		return true;
 	}
 }
